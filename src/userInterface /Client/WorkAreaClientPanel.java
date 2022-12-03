18df25538211a5312ac74ed7f774d654de464e71
adatiyan <adatiya.n@northeastern.edu>
2022-12-03 17:53:32 -0500

    Editing Work Area

--- src/userInterface /Client/WorkAreaClientPanel.java
+++ src/userInterface /Client/WorkAreaClientPanel.java
@@ -82,7 +82,108 @@ public class WorkAreaClientPanel extends javax.swing.JPanel {
         jLabel5.setText("Email");
         add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(284, 201, 55, -1));
 
-      
+        usernametextfield.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernametextfieldActionPerformed(evt);
+            }
+        });
+        add(usernametextfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(381, 114, 99, -1));
+
+        passwordtextfield.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordtextfieldActionPerformed(evt);
+            }
+        });
+        add(passwordtextfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(381, 155, 99, -1));
+        add(emailText, new org.netbeans.lib.awtextra.AbsoluteConstraints(381, 196, 99, -1));
+
+        jButton1.setBackground(new java.awt.Color(159, 159, 201));
+        jButton1.setFont(new java.awt.Font("Avenir", 0, 13)); // NOI18N
+        jButton1.setText("Generate OTP");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(317, 237, -1, -1));
+
+        jButton2.setBackground(new java.awt.Color(159, 159, 201));
+        jButton2.setFont(new java.awt.Font("Avenir", 0, 13)); // NOI18N
+        jButton2.setText("Login");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(284, 288, -1, -1));
+
+        jButton3.setBackground(new java.awt.Color(159, 159, 201));
+        jButton3.setFont(new java.awt.Font("Avenir", 0, 13)); // NOI18N
+        jButton3.setText("Register");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(403, 288, -1, -1));
+
+        jLabel1.setText("jLabel1");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(2, -3, 800, 400));
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if (emailText.getText().isEmpty() || !ValidationUtilities.validateEmail(emailText.getText())) {
+
+            JOptionPane.showMessageDialog(this, "Email not correct");
+            return;
+        }
+        otp otp = new otp();
+        String otpGenerated = otp.getOTP(4);
+        SendEmail.sendMail(otpGenerated, emailText.getText());
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        userProcessContainer.add("OtpPanel", new otpPanel(userProcessContainer, otpGenerated));
+        layout.next(userProcessContainer);
+    }
+
+    public void validateOtp(boolean f) {
+        if (f) {
+            jButton1.setEnabled(true);
+        } else {
+            jButton1.setEnabled(false);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        UserAcc userAccount = system.getUserAccDirectory().authenticateUser(usernametextfield.getText(), passwordtextfield.getText());
+        if (userAccount == null) {
+            JOptionPane.showMessageDialog(null, "Invalid credentials");
+            return;
+        } else {
+            JOptionPane.showMessageDialog(null, "Login successful");
+            CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+            userProcessContainer.add("RequestLabTestJPanel", new ClientLoginWorkAreaPanel(userProcessContainer, system, userAccount, network));
+
+            //  userProcessContainer.add("RequestLabTestJPanel", new WorkAreaClientPanel(userProcessContainer, system, userAcc, network));
+            layout.next(userProcessContainer);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        userProcessContainer.add("RequestLabTestJPanel", new WorkAreaClientRegisterPanel(userProcessContainer, system));
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void usernametextfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernametextfieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_usernametextfieldActionPerformed
+
+    private void passwordtextfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordtextfieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_passwordtextfieldActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
