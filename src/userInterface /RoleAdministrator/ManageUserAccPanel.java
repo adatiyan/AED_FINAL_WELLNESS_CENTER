8a9356a6375e889796de186efcb9fd27fc7593f3
adatiyan <adatiya.n@northeastern.edu>
2022-12-06 20:27:21 -0500

    User Account derivation

---
+++ src/userInterface /RoleAdministrator/ManageUserAccPanel.java
@@ -0,0 +1,168 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package userInterface.RoleAdministrator;
+
+import work.EcoSystem;
+import work.Staff.Staff;
+import work.Enterprise.Enterprise;
+import work.Organization.Organization;
+import work.Role.Role;
+import work.UserAcc.UserAcc;
+import work.UserAcc.UserAccDirectory;
+import java.awt.CardLayout;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class ManageUserAccPanel extends javax.swing.JPanel {
+
+    /**
+     * Creates new form ManageUserAcc
+     */
+    public ManageUserAccPanel() {
+        initComponents();
+    }
+    private JPanel container;
+    private Enterprise enterprise;
+    private UserAccDirectory userAccDir;
+    private Organization organization;
+    
+    public ManageUserAccPanel(JPanel container, Enterprise enterprise) {
+        initComponents();
+        
+        this.enterprise = enterprise;
+        this.container = container;
+
+        popOrganizationComboBox();
+        popData();
+        
+    }
+    
+    public void popOrganizationComboBox() {
+        organizationJComboBox.removeAllItems();
+
+        for (Organization organization : enterprise.getOrganizationDirectory().getOrganizationList()) {
+            organizationJComboBox.addItem(organization);
+        }
+    }
+
+    public void populateEmployeeComboBox(Organization organization) {
+        employeeJComboBox.removeAllItems();
+
+        for (Staff staff : organization.getStaffDirectory().getStaffList()) {
+            employeeJComboBox.addItem(staff);
+        }
+    }
+
+    private void populateRoleComboBox(Organization organization) {
+        roleJComboBox.removeAllItems();
+        for (Role role : organization.getSupportedRole()) {
+            roleJComboBox.addItem(role);
+        }
+    }
+
+    public void popData() {
+
+        DefaultTableModel model = (DefaultTableModel) userJTable.getModel();
+
+        model.setRowCount(0);
+
+        for (Organization organization : enterprise.getOrganizationDirectory().getOrganizationList()) {
+            for (UserAcc ua : organization.getUserAccDirectory().getUserAccList()) {
+                Object row[] = new Object[2];
+                row[0] = ua;
+                row[1] = ua.getRole();
+                ((DefaultTableModel) userJTable.getModel()).addRow(row);
+            }
+        }
+    }
+    
+    
+        organizationJComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        organizationJComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                organizationJComboBoxActionPerformed(evt);
+            }
+        });
+
+        employeeJComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        employeeJComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                employeeJComboBoxActionPerformed(evt);
+            }
+        });
+
+        roleJComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        roleJComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                roleJComboBoxActionPerformed(evt);
+            }
+        });
+
+        createUserJButton.setBackground(new java.awt.Color(0, 204, 204));
+        createUserJButton.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        createUserJButton.setForeground(new java.awt.Color(255, 255, 255));
+        createUserJButton.setText("Create");
+        createUserJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createUserJButtonActionPerformed(evt);
+            }
+        });
+
+        
+        }
+    }//GEN-LAST:event_organizationJComboBoxActionPerformed
+
+    private void employeeJComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_employeeJComboBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_employeeJComboBoxActionPerformed
+
+    private void roleJComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_roleJComboBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_roleJComboBoxActionPerformed
+
+    private void createUserJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createUserJButtonActionPerformed
+        String userName = nameJTextField.getText();
+        String password = passwordJTextField.getText();
+        Organization organization = (Organization) organizationJComboBox.getSelectedItem();
+        Staff staff = (Staff) employeeJComboBox.getSelectedItem();
+        Role role = (Role) roleJComboBox.getSelectedItem();
+        if (!userName.isEmpty() && !password.isEmpty()) {
+            if (organization.getUserAccDirectory().checkIfUsernameIsUnique(userName)) {
+                organization.getUserAccDirectory().createUserAcc(userName, password, staff, role);
+                popData();
+            } else {
+                JOptionPane.showMessageDialog(this, "User name already exists");
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Please enter all fields");
+        }
+        nameJTextField.setText("");
+        passwordJTextField.setText("");
+    }//GEN-LAST:event_createUserJButtonActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton backjButton1;
+    private javax.swing.JButton btn_delete;
+    private javax.swing.JButton createUserJButton;
+    private javax.swing.JComboBox employeeJComboBox;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField nameJTextField;
+    private javax.swing.JComboBox organizationJComboBox;
+    private javax.swing.JTextField passwordJTextField;
+    private javax.swing.JComboBox roleJComboBox;
+    private javax.swing.JTable userJTable;
+    // End of variables declaration//GEN-END:variables
+}
