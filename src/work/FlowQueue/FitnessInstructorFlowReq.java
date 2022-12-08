1cdf65bbe9162639f5a41ce38a10efa9c5a00435
adatiyan <adatiya.n@northeastern.edu>
2022-12-08 11:48:20 -0500

    Flow Request roles

---
+++ src/work/FlowQueue/FitnessInstructorFlowReq.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class FitnessInstructorFlowReq extends FlowReq{
+      private String FitnessInstructorAppointment;
+
+    
+    public String getGymInstructorAppointment() {
+        return FitnessInstructorAppointment;
+    }
+
+    public void setFitnessInstructorAppointment(String FitnessInstructorAppointment) {
+        this.FitnessInstructorAppointment= FitnessInstructorAppointment;
+    }
+
+    
+}
---
+++ src/work/FlowQueue/FlowQueue.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class FlowQueue {
+     private ArrayList<FlowReq> flowReqList;
+
+    public FlowQueue() {
+        flowReqList = new ArrayList();
+    }
+
+    public ArrayList<FlowReq> getflowReqList() {
+        return flowReqList;
+    }   
+    
+}
