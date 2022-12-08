6895a3ef39625fb1aaf8acd767e57e6eae8f1896
adatiyan <adatiya.n@northeastern.edu>
2022-12-07 19:18:44 -0500

    Counsellor role

---
+++ src/work/FlowQueue/CounsellorFlowReq.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class CounsellorFlowReq extends FlowReq{
+     private String CounsellorAppointment;
+
+    public String getCounsellorAppointment() {
+        return CounsellorAppointment;
+    }
+
+    public void setCounsellorAppointment(String CounsellorAppointment) {
+        this.CounsellorAppointment = CounsellorAppointment;
+    }
+    
+}
