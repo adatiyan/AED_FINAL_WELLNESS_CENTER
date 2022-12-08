a92c7c9d2e6899b13add866c1c6dbb0abeed1eb6
adatiyan <adatiya.n@northeastern.edu>
2022-12-08 07:19:27 -0500

    Doctor Flow  role

---
+++ src/work/FlowQueue/DoctorFlowReq.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class DoctorFlowReq extends FlowReq{
+     private String DoctorAppointment;
+
+    public String getDoctorAppointment() {
+        return DoctorAppointment;
+    }
+
+    public void setDoctorAppointment(String DoctorAppointment) {
+        this.DoctorAppointment = DoctorAppointment;
+    }
+    
+}
