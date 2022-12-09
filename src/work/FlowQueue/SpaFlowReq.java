3e8f3971d6e5fb9084406b3361b4e088ed9d4a8a
adatiyan <adatiya.n@northeastern.edu>
2022-12-09 14:19:21 -0500

    Flow Request

---
+++ src/work/FlowQueue/SpaFlowReq.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class SpaFlowReq extends FlowReq {
+    private String SpaAppointment;
+
+    public String getSpaAppointment() {
+        return SpaAppointment;
+    }
+
+    public void setSpaAppointment(String SpaAppointment) {
+        this.SpaAppointment = SpaAppointment;
+    }
+}
---
+++ src/work/FlowQueue/YogaInstructorFlowReq.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work.FlowQueue;
+
+/**
+ *
+ * @author naiyaadatiya
+ */
+public class YogaInstructorFlowReq extends FlowReq{
+     private String YogaInstructorAppointment;
+
+    public String getYogaInstructorAppointment() {
+        return YogaInstructorAppointment;
+    }
+
+    public void setYogaInstructorAppointment(String YogaInstructorAppointment) {
+        this.YogaInstructorAppointment = YogaInstructorAppointment;
+    }  
+    
+}
